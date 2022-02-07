061ea21309e81a7335276c89a80f188d4008e69a
spring-backend-mobdev <enieto@mobdev.cl>
2022-02-06 22:02:13 -0300

    Initial commit

---
+++ src/main/java/cl/mobdev/challenge/RickandmortyV3Application.java
@@ -0,0 +1,13 @@
+package cl.mobdev.challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickandmortyV3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickandmortyV3Application.class, args);
+	}
+
+}
---
+++ src/main/java/cl/mobdev/challenge/controller/RickAndMortyController.java
@@ -0,0 +1,10 @@
+package cl.mobdev.challenge.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RickAndMortyController {
+
+
+    
+}
